3bd9c656ec559a3f85e3a2b85c25f608be5cc3cd
EmFey <manufety15@gmail.com>
2022-09-07 12:00:32 +0200

    Added functions for the operations

---
+++ app.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
