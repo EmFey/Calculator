98d63cac7888f22c9167b9262835102ac1a8f5cf
EmFey <manufety15@gmail.com>
2022-09-08 18:47:46 +0200

    Console logged the results of the functions to check if it works

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 function sum(a, b) {
-    return a + b;
+    return console.log(a + b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return console.log(a - b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return console.log(a * b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return console.log(a / b);
 }
+
+sum(5, 2);
+subtract(5, 2);
+multiply(5, 2);
+divide(4, 2);
